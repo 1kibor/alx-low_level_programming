be46f1e27c29dd3d9aafbb99a1480b4762777f1e
1kibor <ronnykiplangat14@gmail.com>
2023-10-23 12:45:28 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _memset - function that fills memory with a constant byte.
- * @s: source string
- * @b: the contant byte for filling
+ * _memcpy - function fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
  * @n: length of buffer
  * Return: new string.
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-		*(s + i) = b;
+		*(dest + i) = *(src + i);
 		i++;
 	}
-	return (s);
+	return (dest);
 }
