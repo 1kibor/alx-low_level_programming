431b614a52622a28bd94305af7e0079b79101d57
1kibor <ronnykiplangat14@gmail.com>
2023-10-25 02:48:12 -0700

    Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	int a, b, digit, digits, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+	{
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	a = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		a++;
+	}
+	b = power;
+	while (b >= 1)
+	{
+		numchar = (temp / b) % 10;
+		_putchar(numchar + '0');
+		b = b / 10;
+	}
+}
