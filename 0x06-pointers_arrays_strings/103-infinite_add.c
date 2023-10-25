2c3573d512e6ce995889dc13cebe199aeeb1557e
1kibor <ronnykiplangat14@gmail.com>
2023-10-25 04:09:37 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_number(int n)
 {
-	int lengA = 0, lengB = 0, carry = 0, a, b, sum, biggest;
-	
-	while (n1[lengA] != '\0')
-		lengA++;
-	while (n2[lengB] != '\0')
-		lengB++;
-	if (lengA > lengB)
-		biggest = lengA;
-	else
-		biggest = lengB;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-	
-	while (biggest >= 0)
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
+	if (n < 0)
 	{
-		a = (n1[lengA - 1] - '0');
-		b = (n1[lengB - 1] - '0');
-		if (lengA > 0 && lengB > 0)
-			sum = a + b + carry;
-		else if (lengA < 0 && lengB > 0)
-			sum = b + carry;
-		else if (lengA > 0 && lengB < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-		
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		lengA--;
-		lengB--;
-		biggest--;
+		_putchar('-');
+		temp = -n;
 	}
-	if (*(r) != 0)
-		return (r);
 	else
-		return (r + 1);
+	{
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
 }
