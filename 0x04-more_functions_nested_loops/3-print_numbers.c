2c8da3355c6a1ce5923ece5974a52e3e3e9a8814
otieron <kibor259@gmail.com>
2024-01-10 00:19:17 -0800

    numbers have life

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 */
 void print_numbers(void)
 {
-int num = 0;
-for (num = 0; num < 10; num++)
+int number = 48;
+for (number = 48; number < 58; number++)
 {
-_putchar(num);
+_putchar(number);
 }
 _putchar(10);
 }
