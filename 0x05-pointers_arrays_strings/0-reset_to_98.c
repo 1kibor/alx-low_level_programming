7975355d8f402ce73c3f93a1f3f99d79bc15b99b
otieron <kibor259@gmail.com>
2024-01-12 09:02:00 -0800

    98 battery St.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n:the pointer to the variable 
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
