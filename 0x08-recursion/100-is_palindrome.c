5f4ebc6bc686f70c391c31b507b4c217b493d7b2
otieron <kibor259@gmail.com>
2024-01-12 02:32:59 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * palindrome - obtains length of a
+ * palind - obtains length of a
  * @a: string
  * @l: integer to count length
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int palindrome(char *s, int l)
+int palind(char *a, int l)
 {
-	if (*s == 0)
+	if (*a == 0)
 		return (l - 1);
-	return (palindrome(s + 1, l + 1));
+	return (palind(a + 1, l + 1));
 }
 /**
  * palind1 - compares string vs string reverse
@@ -22,13 +22,13 @@ int palindrome(char *s, int l)
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int palind1(char *s, int l)
+int palind1(char *a, int l)
 {
-	if (*s != *(s + l))
+	if (*a != *(a + l))
 		return (0);
-	else if (*s == 0)
+	else if (*a == 0)
 		return (1);
-	return (palind1(s + 1, l - 2));
+	return (palind1(a + 1, l - 2));
 }
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -37,10 +37,10 @@ int palind1(char *s, int l)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int is_palindrome(char *a)
+int is_palindrome(char *s)
 {
 	int l;
 
-	l = palind2(a, 0);
-	return (palind1(a, l));
+	l = palind(s, 0);
+	return (palind1(s, l));
 }
