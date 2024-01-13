c78b73d7ccfc812ff8350aed3e6e98bc15b3a700
otieron <kibor259@gmail.com>
2024-01-13 01:11:16 -0800

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - fxn that prints half of a string, 
+ *followed by a new line
+ *
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int p, n, y;
+
+	p = 0;
+
+	while (str[p] != '\0')
+	{
+		p++;
+	}
+
+	if (p % 2 == 0)
+	{
+		for (y = p / 2; str[y] != '\0'; y++)
+		{
+			_putchar(str[y]);
+		}
+	} else if (p % 2)
+	{
+		for (n = (p - 1) / 2; n < p - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
