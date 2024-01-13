285ef31005bd5a8229a28aaeb8d57f094b3f181e
otieron <kibor259@gmail.com>
2024-01-13 00:34:04 -0800

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
-/*
- * _puts - write a function that prints a string, followed by a new line
+/**
+ * _puts - a function that prints a string, followed by a new line
  *
- * @str: pointer to the string to print
+ *@str: pointer to the string to print
  *
  */
 void _puts(char *str)
 {
-	int i;
+	int index;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(str[i]);
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
-
