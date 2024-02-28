c9ef16ca73db3a153faaec3a4b9b9583d4225c3b
1kibor <kibor259@gmail.com>
2024-02-28 14:05:16 -0800

    101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,15 +10,14 @@
 */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-
 {
-	unsigned long int xor_result = n ^ m;
-	unsigned int flip_count = 0;
+unsigned long int xor_result = n ^ m;
+unsigned int flip_count = 0;
 while (xor_result > 0)
 {
 if ((xor_result & 1) == 1)
-	flip_count++;
-	xor_result >>= 1;
+flip_count++;
+xor_result >>= 1;
 }
 return (flip_count);
 }
