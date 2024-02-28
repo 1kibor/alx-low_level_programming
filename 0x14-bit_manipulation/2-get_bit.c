1688d7d50b3d05a47dac89048ec331009f998806
1kibor <kibor259@gmail.com>
2024-02-28 13:57:10 -0800

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_bit - a function that gets a bit at index
- * @n: the number to get bit from
- * @index: the index where the bit get at
- *
- * Return: value of the bit or -1 if an error occured
- */
+* get_bit - A function that gets a bit at index
+* @n: The number to get bit from
+* @index: The index where the bit get at
+*
+* Return: The value of the bit or -1 if an error occured
+*/
 
 int get_bit(unsigned long int n, unsigned int index)
 {
